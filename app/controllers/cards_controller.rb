ecd1912c583bb28a174b0656fbf5c0b7a5aaffe4
Fred Agyeman <fredyliby@verizon.net>
2015-11-02 15:47:28 -0500

    modified card validations

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,28 +1,35 @@
 class CardsController < ApplicationController
-
 	
 	def index
-	    if params[:user_id].present?
-	      @user = User.find params[:user_id]
-	      @cards = @user.cards
-	      else
-	      @cards = Card.all
-	    end	      
+		if params[:user_id].present?
+			@user = User.find params[:user_id]
+			@cards = @user.cards
+		else
+			@cards = Card.all
+		end	      
 	end
 
 
 	def new
-	  	@card = Card.new
+		@card = Card.new
 	end
 
 	def create
-	  	@card = Card.new(card_params)
-	  	@card.save!
-	  	redirect_to @card
+		@card = Card.new(card_params)
+
+		if @card.valid?
+			@card.save!
+			@card.users << current_user	
+			redirect_to cards_path
+		else
+			flash[:alert] = "Problem with card: #{@card.errors.full_messages.first}"
+			render 'new'
+		end
+
 	end	 
 
 	def show
-	  	@card = Card.find(params[:id])
+		@card = Card.find(params[:id])
 	end
 
 	def destroy
@@ -34,9 +41,9 @@ class CardsController < ApplicationController
 	private
 
 	def card_params
- 		params.require(:card).permit(:number, :expiration, :month, :year)
+		params.require(:card).permit(:number, :expiration, :month, :year)
 	end
-	
+
 end
 
 
--- app/models/card.rb
+++ app/models/card.rb
@@ -4,12 +4,14 @@ class Card < ActiveRecord::Base
   validates :number, :uniqueness => true
 
 
-  validates :users, presence: true
 
-	validates :number, format: { with: /\d{4}-\d{4}-\d{4}-\d{4}/}
+	validates :year, inclusion: {in: (2015..2115)}
+  # validates_format_of :number, with: '/^3[47][0-9]{13}$/'
+  # /\A[345]\d{11,}\Z/
+  validates :number, length: { is: 16 }
+
   validates :month, inclusion: {in: (1..12)}
-  validates :year, inclusion: {in: (2015..2115)}
-
+  
   # before_save :set_card_type, :set_expiration_date
 
   def set_expiration_date
