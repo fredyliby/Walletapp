1b54cb3c1bdbc443e131479b0d9d353900e179e4
Fred Agyeman <fredyliby@verizon.net>
2015-12-11 02:57:51 -0500

    modified show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
 	def create
-       user = User.find_by_email(params[:email])
-       if user.password == params[:password]
-        session[:user_id] = user.id
-        flash[:welcome] = "valid Information"
-        redirect_to user_path (user.id)
-       else
-      flash[:alert] = "Invalid Informations"
-      redirect_to new_session_path
-       end
+   user = User.find_by_email(params[:email])
+   if user.password == params[:password]
+    session[:user_id] = user.id
+    flash[:welcome] = "valid Information"
+    redirect_to user_path (user.id)
+  else
+    flash[:alert] = "Invalid Informations"
+    redirect_to new_session_path
   end
+end
 
- def destroy
- 	  session.clear
-      flash[:notice] = "Logged Out"
-      redirect_to root_path
-  end
+def destroy
+  session.clear
+  flash[:notice] = "Logged Out"
+  redirect_to root_path
+end
 
 end
 
